6bb95f7987fe4aa8df2a36dbd69889737981ff4f
Stanislav7110 <snage873@gmail.com>
2026-09-14 03:06:45 +0300

    Initialize Supabase client with URL and key

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+const SUPABASE_URL = "https://kfhlvfymyyqgtsamgcra.supabase.co";
 
+// ВСТАВЬ СЮДА свой Publishable key из Supabase
+const SUPABASE_KEY = "sb_publishable_GKFUCUwNTj4m-FRGL2Pm2g_i1DI1xkk";
+
+const { createClient } = supabase;
+
+const supabaseClient = createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
+
+console.log("Supabase подключён");
